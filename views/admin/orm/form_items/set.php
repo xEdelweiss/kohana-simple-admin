619d5f12268d1bf07f94ebbe49eef7027109ac01
Michael Sverdlikovsky <xedelweiss@gmail.com>
2013-11-28 17:30:40 +0200

    select for set field in case if options count is gt 3

--- views/admin/orm/form_items/set.php
+++ views/admin/orm/form_items/set.php
@@ -1,16 +1,26 @@
 <?php /** @var ORM $object */ /** @var ORM $model */ ?>
 <?php $element_id = $object->object_name().'_'.$object->pk().'_'.$column->column_name ?>
 <?php $element_name = $column->column_name; ?>
-<?php // @todo add combobox if options count > 3 ?>
 
 <div class="form-group">
     <label class="col-md-4 control-label" for="<?=$element_name?>"><?= $column->column_name ?></label>
     <div class="col-md-4">
-        <?php foreach ($column->options as $k => $option): ?>
-            <label class="radio-inline" for="<?=$element_id?>_<?=$k?>">
-                <input type="radio" name="<?=$element_name?>" id="<?=$element_id?>_<?=$k?>" value="<?= $option ?>" <?= $object->{$column->column_name} == $option ? 'checked="checked"' : ''?>>
-                <?= $option ?>
-            </label>
-        <?php endforeach; ?>
+        <!-- select -->
+        <?php if (count($column->options) > 3): ?>
+            <select id="<?=$element_id?>" name="<?=$element_name?>" class="form-control">
+                <?php foreach ($column->options as $k => $option): ?>
+                    <option value="<?= $option ?>" <?= $object->{$column->column_name} == $option ? 'selected="selected"' : ''?>><?= $option ?></option>
+                <?php endforeach; ?>
+            </select>
+
+        <!-- radios -->
+        <?php else: ?>
+            <?php foreach ($column->options as $k => $option): ?>
+                <label class="radio-inline" for="<?=$element_id?>_<?=$k?>">
+                    <input type="radio" name="<?=$element_name?>" id="<?=$element_id?>_<?=$k?>" value="<?= $option ?>" <?= $object->{$column->column_name} == $option ? 'checked="checked"' : ''?>>
+                    <?= $option ?>
+                </label>
+            <?php endforeach; ?>
+        <?php endif; ?>
     </div>
 </div>
