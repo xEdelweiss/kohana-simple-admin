063b65bc5f1f07fa82246a062874370bded99142
Michael Sverdlikovsky <xedelweiss@gmail.com>
2013-11-28 17:44:12 +0200

    min/max value/length + tinyint field

--- views/admin/orm/form_items/tinyint.php
+++ views/admin/orm/form_items/tinyint.php
@@ -5,6 +5,6 @@
 <div class="form-group">
     <label for="<?=$element_id?>" class="col-sm-2 control-label"><?= $column->column_name ?></label>
     <div class="col-sm-10">
-        <input type="number" class="form-control" id="<?=$element_id?>" name="<?=$element_name?>" value="<?= $object->{$column->column_name} ?>" min="<?=$column->min?>" max="<?=$column->max?>"  />
+        <input type="number" class="form-control" id="<?=$element_id?>" name="<?=$element_name?>" value="<?= $object->{$column->column_name} ?>" min="<?=$column->min?>" max="<?=$column->max?>" />
     </div>
 </div>
--- views/admin/orm/form_items/varchar.php
+++ views/admin/orm/form_items/varchar.php
@@ -6,9 +6,9 @@
     <label for="<?=$element_id?>" class="col-sm-2 control-label"><?= $column->column_name ?></label>
     <div class="col-sm-10">
         <?php if ($column->character_maximum_length > 50): ?>
-            <textarea class="form-control" rows="4" id="<?=$element_id?>" name="<?=$element_name?>"><?= $object->{$column->column_name} ?></textarea>
+            <textarea class="form-control" rows="4" id="<?=$element_id?>" name="<?=$element_name?>" maxlength="<?=$column->character_maximum_length?>"><?= $object->{$column->column_name} ?></textarea>
         <?php else: ?>
-            <input type="text" class="form-control" id="<?=$element_id?>" name="<?=$element_name?>" value="<?= $object->{$column->column_name} ?>" />
+            <input type="text" class="form-control" id="<?=$element_id?>" name="<?=$element_name?>" value="<?= $object->{$column->column_name} ?>" maxlength="<?=$column->character_maximum_length?>" />
         <?php endif; ?>
     </div>
 </div>
