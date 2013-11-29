d85a6aed725bd7b7dcd582fb8d6860d91a2e64fa
Michael Sverdlikovsky <xedelweiss@gmail.com>
2013-11-29 12:58:50 +0200

    double field

---
+++ views/admin/orm/form_items/double.php
@@ -0,0 +1,10 @@
+<?php /** @var ORM $object */ /** @var ORM $model */ ?>
+<?php $element_id = $object->object_name().'_'.$object->pk().'_'.$column->column_name ?>
+<?php $element_name = $column->column_name; ?>
+
+<div class="form-group">
+    <label for="<?=$element_id?>" class="col-sm-2 control-label"><?= $column->column_name ?></label>
+    <div class="col-sm-10">
+        <input type="number" class="form-control" id="<?=$element_id?>" name="<?=$element_name?>" value="<?= $object->{$column->column_name} ?>" step="0.05"/>
+    </div>
+</div>
